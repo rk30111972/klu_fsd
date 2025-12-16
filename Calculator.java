7b8486398bf40ea3c5c3bc37970830f8bd667c0e
rk30111972 <rk30111972@gmail.com>
2025-12-16 12:02:48 +0530

    Calculator

---
+++ Calculator.java
@@ -0,0 +1,8 @@
+package Calculator_Project;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        System.out.println("Addition: " + (a + b));
+    }
+}
